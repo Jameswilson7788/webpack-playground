1e4cc0fdffb78f50042c79bc9a8f0ec19bc00e3f
JamesWilson <wesstation2@gmail.com>
2017-10-22 21:55:27 +0800

    webpack的基本輸入輸出

---
+++ api.js
@@ -0,0 +1,18 @@
+module.exports = {
+  getUsers:function() {
+    return [
+      {
+        name:'Weigo',
+        age: 25,
+      },
+      {
+        name:'Annie',
+        age: 30
+      },
+      {
+        name:'Penny',
+        age: 23
+      }
+    ]
+  }
+}
